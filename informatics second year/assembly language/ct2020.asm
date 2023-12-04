2996dba9e2aad3623c25fa5f675add3e32c98dc7
齐哈 <Cherazade1412@protonmail.com>
2023-12-04 18:09:15 +0100

    ct2020

--- informatics second year/assembly language/ct2020.asm
+++ informatics second year/assembly language/ct2020.asm
@@ -1,11 +1,11 @@
-	.ORIG X3000     
-	LD R0,char
-	AND R1,R1,#0
-loop:	ADD R2,R1,#-9
-	BRp endloop
-	OUT
-	ADD R1,R1,#1
-	BR loop
-endloop: HALT
-char:	.FILL 90
-.END
+	.ORIG X3000
+	LD R1, a
+	ADD R2,R1,R1
+	ADD R3,R2,R2
+	ADD R3,R3,R2
+	ADD R3,R3,R1
+	ST R3,resultat
+	HALT
+a:	.FILL 2
+resultat:	.BLKW 1
+	.END
