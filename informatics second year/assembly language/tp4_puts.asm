5c0b13529d7ffef36f8c1b0de9bc82be0bde9a86
齐哈 <Cherazade1412@protonmail.com>
2023-12-06 09:51:41 +0100

    assembly

---
+++ informatics second year/assembly language/tp4_puts.asm
@@ -0,0 +1,11 @@
+	.ORIG X3000     ; spécifie l'adresse de chargement du programme
+	LD R0,char
+	AND R1,R1,#0
+loop:	ADD R2,R1,#-9
+	BRp endloop
+	OUT
+	ADD R1,R1,#1
+	BR loop
+endloop: HALT
+char:	.FILL 90
+.END
